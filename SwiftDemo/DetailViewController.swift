3e9937b671d2a7a9d5c7eb2debda62aaf7c5ac1e
Wind382690308 <luc@www.local>
2016-08-31 17:32:47 +0800

    继承baseClass

--- SwiftDemo/DetailViewController.swift
+++ SwiftDemo/DetailViewController.swift
@@ -27,7 +27,10 @@ class DetailViewController: UIViewController {
         
 //        LearnFunctionData()
         
-        LearnSubscriptData()
+//        LearnSubscriptData()
+        
+        LearnBaseClassData()
+        
         
     }
     
@@ -651,6 +654,106 @@ class DetailViewController: UIViewController {
      学习下标脚本
      */
     func LearnSubscriptData() -> Void {
+        
+//        下标脚本 可以定义在类（Class）、结构体（structure）和枚举（enumeration）这些目标中，可以认为是访问对象、集合或序列的快捷方式，不需要再调用实例的特定的赋值和访问方法。
+//        举例来说，用下标脚本访问一个数组(Array)实例中的元素可以这样写 someArray[index] ，访问字典(Dictionary)实例中的元素可以这样写 someDictionary[key]。
+//        对于同一个目标可以定义多个下标脚本，通过索引值类型的不同来进行重载，而且索引值的个数可以是多个。
+//        下标脚本语法及应用
+//        语法
+//        下标脚本允许你通过在实例后面的方括号中传入一个或者多个的索引值来对实例进行访问和赋值。
+//        语法类似于实例方法和计算型属性的混合。
+//        与定义实例方法类似，定义下标脚本使用subscript关键字，显式声明入参（一个或多个）和返回类型。
+//        与实例方法不同的是下标脚本可以设定为读写或只读。这种方式又有点像计算型属性的getter和setter：
+        
+//        ep1
+        struct subexample{
+        
+            let decrmmenter : Int
+            
+            //下标脚本允许你通过在实例后面的方括号中传入一个或者多个的索引值来对实例进行访问和赋值。
+            subscript (index : Int) -> Int {
+            
+                
+                return decrmmenter / index
+            }
+            
+        }
+        
+        var divvision = subexample.init(decrmmenter: 100)
+        
+        print("100 / 10 等于 \(divvision[10])")
+        
+//        ep2
+        
+        class daysofaweek{
+        
+          private   var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                                "Thursday", "Friday", "saturday"]
+        
+            
+            subscript (index:Int) ->String{
+            
+                set {
+                
+                    var chooseIndex = index
+                    
+                    if chooseIndex>days.count {
+                    
+                        chooseIndex = days.count-1
+                    }
+                    
+                    self.days[chooseIndex] = newValue
+                    
+                }
+                
+                get {
+                    
+                    var chooseIndex = index
+                    
+                    if chooseIndex>days.count {
+                        
+                        chooseIndex = days.count-1
+                    }
+                    
+                
+                     return ( days[chooseIndex] )
+                }
+                
+            }
+            
+        }
+        
+        var  p = daysofaweek()
+        
+        print(p[100]) //也会越界
+        
+//        用法
+//        根据使用场景不同下标脚本也具有不同的含义。
+//        通常下标脚本是用来访问集合（collection），列表（list）或序列（sequence）中元素的快捷方式。
+//        你可以在你自己特定的类或结构体中自由的实现下标脚本来提供合适的功能。
+//        例如，Swift 的字典（Dictionary）实现了通过下标脚本对其实例中存放的值进行存取操作。在下标脚本中使用和字典索引相同类型的值，并且把一个字典值类型的值赋值给这个下标脚来为字典设值：
+        
+        var animans = ["spider": 8, "ant": 6, "cat": 4]
+        
+        animans["bird"] = 2
+        
+        print(animans)
+        
+        
+//        附属脚本选项
+//        
+//        附属脚本允许任意数量的入参索引，并且每个入参类型也没有限制。附属脚本的返回值也可以是任何类型。附属脚本可以使用变量参数和可变参数，但使用写入读出（in-out）参数或给参数设置默认值都是不允许的。
+//        
+//        一个类或结构体可以根据自身需要提供多个附属脚本实现，在定义附属脚本时通过入参个类型进行区分，使用附属脚本时会自动匹配合适的附属脚本实现运行，这就是附属脚本的重载。
+//        
+//        一个附属脚本入参是最常见的情况，但只要有合适的场景也可以定义多个附属脚本入参。如下例定义了一个Matrix结构体，将呈现一个Double类型的二维矩阵。Matrix结构体的附属脚本需要两个整型参数：
+        
+    }
+    
+    /**
+     学习继承
+     */
+    func LearnBaseClassData() -> Void {
         
         
         
